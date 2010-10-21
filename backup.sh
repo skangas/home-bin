3162494cb043f64d51de74478d0f5e9cfec9ca1d
Stefan Kangas <skangas@skangas.se>
2010-10-21 05:48:31 +0200

    Various changes

--- backup.sh
+++ backup.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
-sudo rsync -auvx -E -H --delete --ignore-errors / /home /var /usr /home/skangas/windows /mnt/backup/slash/
+sudo mount /mnt/backup/
+mount | grep "on /mnt/backup type"
+
+if [[ "$?" == 0 ]]; then
+    sudo rsync -auvx -E -H --delete --ignore-errors \
+    / /home /var /usr /home/skangas/windows /mnt/backup/slash/
+else
+    exit 1
+fi
 
