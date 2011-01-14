0326bff283e943d4c38a3325f186f017fd929669
Stefan Kangas <skangas@skangas.se>
2011-01-14 12:57:59 +0100

    Fix plagger.sh

--- plagger.sh
+++ plagger.sh
@@ -1,5 +1,7 @@
-#!/bin/bash
+#!/bin/bash -l
 
-/home/skangas/src/plagger/plagger --config=/home/skangas/src/plagger/config/no-eft.yaml
-/home/skangas/src/plagger/plagger --config=/home/skangas/src/plagger/config/with-eft.yaml
+export PERL5LIB=/home/skangas/usr/lib/perl5
+
+/home/skangas/src/plagger/plagger --config=/home/skangas/src/plagger/config/no-eft.yaml &> /tmp/plagger_log1
+/home/skangas/src/plagger/plagger --config=/home/skangas/src/plagger/config/with-eft.yaml &> /tmp/plagger_log2
 #/home/skangas/src/plagger/plagger --config=/home/skangas/src/plagger/config/vansterpartiet.yaml
