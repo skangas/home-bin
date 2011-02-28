addf19b8de703143f532591fe82f4554c1abc82e
Stefan Kangas <skangas@skangas.se>
2011-02-28 23:08:53 +0100

    add .gitignore

--- plagger.sh
+++ plagger.sh
@@ -2,6 +2,8 @@
 
 export PERL5LIB=/home/skangas/usr/lib/perl5
 
+set +o noclobber
+
 /home/skangas/src/plagger/plagger --config=/home/skangas/src/plagger/config/no-eft.yaml &> /tmp/plagger_log1
 /home/skangas/src/plagger/plagger --config=/home/skangas/src/plagger/config/with-eft.yaml &> /tmp/plagger_log2
 #/home/skangas/src/plagger/plagger --config=/home/skangas/src/plagger/config/vansterpartiet.yaml
