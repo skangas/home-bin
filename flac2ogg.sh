84a38587ee022e6ab88708fa8b85c31ace3b7b8d
Stefan Kangas <skangas@skangas.se>
2011-08-15 11:43:17 +0200

    Add flac2ogg.sh

---
+++ flac2ogg.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# display cruft
+find -type f -iname "*.!ut"
+
+# remove cruft
+find -type f \( -iname "*.m3u" -or -iname "*.m3u8" -or -iname "*.cue" -or -iname "*.sfv" \) -delete
+
+# encode
+find -type f -iname "*.flac" -exec oggenc -q8 {} \; -delete
+
+# vorbisgain
+find -mindepth 1 -type d -exec vorbisgain -a -r -s -f "{}" \;
+
+
