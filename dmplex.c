b27ce63576ab01d3a5ae070e3eab9eead0fc33a7
Stefan Kangas <skangas@skangas.se>
2011-09-22 18:19:27 +0200

    Add dmplex.c

---
+++ dmplex.c
@@ -0,0 +1,81 @@
+/*
+   simple dzen multiplexer
+
+   Copyright (C) 2008 by Robert Manea  <rob dot manea at gmail dot com>
+
+   Permission is hereby granted, free of charge, to any person obtaining a copy
+   of this software and associated documentation files (the "Software"), to deal
+   in the Software without restriction, including without limitation the rights
+   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+   copies of the Software, and to permit persons to whom the Software is
+   furnished to do so, subject to the following conditions:
+
+   The above copyright notice and this permission notice shall be included in
+   all copies or substantial portions of the Software.
+
+   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+   THE SOFTWARE.
+
+Compile:   
+- gcc -Wall dmplex.c -o dmplex
+
+Use:   
+- mkfifo pipe
+- tail -f pipe | dmplex | dzen2
+- echo "1 foo" > pipe
+
+*/
+
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+#define MAX_IN_LEN   8192
+#define MAX_SECTIONS 1024
+
+char tbuf[MAX_IN_LEN];
+char nbuf[MAX_IN_LEN];
+char *section[MAX_SECTIONS];
+
+int main(int argc, char *argv[])
+{
+	int i, snr, slen;
+
+	while(fgets(tbuf, MAX_IN_LEN, stdin)) {
+
+		for(i=0; i<MAX_IN_LEN && tbuf[i] != ' '; i++) 
+			nbuf[i] = tbuf[i];
+		nbuf[i] = 0;
+		snr = atoi(nbuf);
+
+		if(snr < MAX_SECTIONS) {
+			if(section[snr])
+				free(section[snr]);
+
+			slen = strlen(tbuf+i);
+			section[snr] = calloc(slen, sizeof(char));
+
+			if(tbuf[i+slen-1] == '\n')
+				tbuf[i+slen-1] = 0;
+
+			strncpy(section[snr], tbuf+i+1, slen);
+
+			for(i=0; i<MAX_SECTIONS; i++) {
+				if(section[i])
+					printf("%s", section[i]);
+			}
+			printf("\n");
+			fflush(stdout);
+		}
+	}
+
+
+	return EXIT_SUCCESS;
+}
+
