4f6911da892043c356c49cb50e7e778a68edb972
Stefan Kangas <skangas@skangas.se>
2011-09-27 00:26:34 +0200

    update

--- conky-biff.sh
+++ conky-biff.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
+NEW_MAIL_TOTAL=0
+
+find_new_mail() {
+    FOUND_MAIL=`find $MAILDIR -type f -path '$MAILDIR/*new/*'|wc -l`    
+}
+
+
 MAILDIR="$HOME/Maildir"
 
 if [ ! -d $MAILDIR ]; then
     exit
 fi
 
-FOUND_MAIL=`find $HOME/Maildir -type f -path 'Maildir/*new/*'|wc -l`
+
 
 if [ $FOUND_MAIL -gt 0 ]; then
-    echo "^fg(\#F00) $FOUND_MAIL NEW MAIL"
+    echo "^fg(\#F00) $FOUND_MAIL NEW MAIL ^fg(\#666)|"
 fi
