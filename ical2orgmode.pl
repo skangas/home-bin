a9e660afefdacd8c17b63bc0f7f3171260d2bbd1
Stefan Kangas <skangas@skangas.se>
2012-01-31 03:14:32 +0100

    update

--- ical2orgmode.pl
+++ ical2orgmode.pl
@@ -14,60 +14,7 @@ use Data::Dumper;
 use Data::ICal;
 use DateTime::Format::ICal;
 
-my $cal = Data::ICal->new( filename => $ARGV[0] );
-
-say "* Schema" . (" " x 60) . ":school:";
-
-MAIN:
-for my $entry (@{$cal->entries}) {
-    my %prop = get_properties($entry, qw/summary description location dtend dtstart/);
-    for my $key (keys %prop) {
-        defined $prop{$key} || next MAIN;
-    }
-
-    # Fix stuff for buggy timeedit
-    my $desc = $prop{'description'};
-    $desc =~ s/\n/ /g;
-    $desc =~ s/ +/ /g;
-    my $summary = $desc;
-
-    ### SUMMARY
-    # my $summary = $prop{'summary'};
-    # $summary =~ s/--\n//;
-
-    $summary =~ s/
-                 ( TKITE-2
-                 | TKDAT-2
-                 | MPSEN-1
-                 )
-                 //x;
-    $summary =~ s/TKITE-2//;    
-    $summary =~ s/^(.*)(?:Föreläsning|Förel)/Föreläsning: $1/sx;
-    $summary =~ s/^(.*)(?:Övning|Övn)/Övning: $1/sx;
-    $summary =~ s/^(.*)(?:Handledning|Handleding)/Handledning: $1/sx;
-    $summary =~ s/^(.*)(?:Laboration|Lab)/Laboration: $1/sx;
-
-    # Limit to one space
-    $summary =~ s/\s+/ /g;
-    # Remove whitespace at beginning/end of line
-    $summary =~ s/( ^\s+ | \s+$ )//gix;
-
-    $summary .= ' [' . $prop{'location'} . ']';
-
-    ### DATE
-    my $dt;
-    $dt = DateTime::Format::ICal->parse_datetime($prop{'dtstart'});
-    my $date = $dt->year . "-" . sprintf("%02d", $dt->month) . "-" . sprintf("%02d", $dt->day) . " "
-             . sprintf("%02d", $dt->hour) . ":" . sprintf("%02d", $dt->minute);
-    $dt = DateTime::Format::ICal->parse_datetime($prop{'dtend'});
-    $date .= "-" . sprintf("%02d", $dt->hour) . ":" . sprintf("%02d", $dt->minute);
-    
-    say "*** $summary <$date>";
-    # say "";
-    # say "   :PROPERTIES:";
-    # say "   :Course code: $location:";
-    # say "   :END:\n";        
-}
+our $LocalTZ = DateTime::TimeZone->new( name => 'CET' );
 
 sub get_properties {
     my ($entry, @properties) = @_;
@@ -79,6 +26,73 @@ sub get_properties {
     return %props;
 }
 
+my $filename = $ARGV[0];
+defined $filename || die "No file specified";
+
+my $cal = Data::ICal->new( filename => $filename );
+
+say "* Schema";
+
+MAIN:
+for my $entry (@{$cal->entries}) {
+    my %prop = get_properties($entry, qw/summary description location dtend dtstart/);
+    for my $key (keys %prop) {
+        defined $prop{$key} || next MAIN;
+    }
+
+    # Fix stuff for useless timeedit
+    my $desc = $prop{'description'};
+    $desc =~ s/\n/ /g;
+    my $summary = $desc;
+
+    ### SUMMARY
+    # my $summary = $prop{'summary'};
+    # $summary =~ s/--\n//;
+
+    $summary =~ s/
+                 ( TKITE-2
+                 | TKDAT-2
+                 | MPSEN-1
+                 | MPCSN-1
+                 | MPALG-1
+                 | Real\ time\ systems
+                 | ID\ \d+
+                 ) ,?
+                 //gx;
+    $summary =~ s/TKITE-2//;    
+    # $summary =~ s/^(.*)(?:Föreläsning|Förel)/Föreläsning: $1/sx;
+    # $summary =~ s/^(.*)(?:Övning|Övn)/Övning: $1/sx;
+    # $summary =~ s/^(.*)(?:Handledning|Handleding)/Handledning: $1/sx;
+    # $summary =~ s/^(.*)(?:Laboration|Lab)/Laboration: $1/sx;
+    $summary =~ s/(Förel|Övn),/$1:/;
+
+    # Limit to one space
+    $summary =~ s/\s+/ /g;
+    # Remove whitespace at beginning/end of line
+    $summary =~ s/( ^\s+ | \s+$ )//gix;
+
+    # $summary .= ' [' . $prop{'location'} . ']';
+    $summary .= ' [' . $prop{'summary'} . ']';
+
+    ### DATE
+    my $dt;
+
+    $dt = DateTime::Format::ICal->parse_datetime($prop{'dtstart'}, time_zone => $LocalTZ);
+    $dt->set_time_zone("Europe/Stockholm");
+    my $date = $dt->year . "-" . sprintf("%02d", $dt->month) . "-" . sprintf("%02d", $dt->day) . " "
+             . sprintf("%02d", $dt->hour) . ":" . sprintf("%02d", $dt->minute);
+
+
+    $dt = DateTime::Format::ICal->parse_datetime($prop{'dtend'});
+    $dt->set_time_zone("Europe/Stockholm");
+    $date .= "-" . sprintf("%02d", $dt->hour) . ":" . sprintf("%02d", $dt->minute);
+    
+    say "** $summary <$date>";
+    # say "";
+    # say "   :PROPERTIES:";
+    # say "   :Course code: $location:";
+    # say "   :END:\n";        
+}
 
 =head1 CAVEAT
 
