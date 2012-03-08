ce2a3da4cecc5c14d0fb78b29ed5b79882fcf865
Stefan Kangas <skangas@skangas.se>
2012-03-08 15:34:33 +0100

    update

--- backup.sh
+++ backup.sh
@@ -5,8 +5,7 @@ HOST=`hostname`
 
 # mount as needed
 if [ `mount | grep "on /mnt/backup type" > /dev/null` ]; then
-    # FIXME
-    mount /mnt/backup/
+    /home/skangas/bin/mount-backup.sh
 fi
 
 # do the backup only when mount succeded
@@ -14,12 +13,5 @@ if [ `mount | grep "on /mnt/backup type" > /dev/null` ]; then
     exit 1
 fi
 
-nice -n 19 ionice -c 3 \
-    rsync \
-    -auvx -E -H --delete-before --delete-excluded --ignore-errors --verbose --progress \
-    --exclude=/dev --exclude=/proc --exclude=/sys --exclude=/var/cache/apt/archives --exclude=/var/lib/apt/lists/ \
-    --exclude=/home/skangas/.wine \
-    / /home /usr /var $BACKUP_DIRECTORY/$HOST
-
-
+/usr/bin/rsnapshot $@
 
