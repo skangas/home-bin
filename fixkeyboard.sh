6a00242030828ae6958dd2940b74ecd98e409470
Stefan Kangas <skangas@skangas.se>
2016-06-11 04:32:15 +0200

    add fixkeyboard.sh

---
+++ fixkeyboard.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+setxkbmap se_sv_dvorak
+setxkbmap -option ctrl:nocaps
+xset b off         # no bell
+xset m 2 2         # mouse sensitivity
+xset r rate 300 40 # keybord repeat rate
+
