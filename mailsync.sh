77ad6b4669e9cafb6ef152bd2a6fbb5e559b9463
Stefan Kangas <stefan@marxist.se>
2021-11-08 07:47:17 +0100

    Add mailsync.sh

---
+++ mailsync.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# lock -- see flock(1) for more details
+[ "${FLOCKER}" != "$0" ] && exec env FLOCKER="$0" flock -en "$0" "$0" "$@" || :
+
+# configuration
+MAIL_ROOT=~/.mail/account.gmail/
+GMI=$HOME/src/lieer/gmi
+GMI_FLAGS="--quiet"
+NOTMUCH_FLAGS="--quiet"
+
+# sync mail
+cd $MAIL_ROOT
+$GMI manage_queue --quiet -r -C ~/.mail/account.gmail
+$GMI sync $GMI_FLAGS
+notmuch new $NOTMUCH_FLAGS
