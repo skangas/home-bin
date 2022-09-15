170b1f450798c0738b806719ab18d7a96f684190
Stefan Kangas <stefankangas@gmail.com>
2022-09-15 21:00:09 +0200

    automatic update @ joffe

--- mailsync.sh
+++ mailsync.sh
@@ -6,15 +6,16 @@
 # configuration
 MAIL_ROOT=~/.mail/account.gmail/
 GMI=$HOME/src/lieer/gmi
-GMI_FLAGS=""
-#GMI_FLAGS="--quiet"
-NOTMUCH_FLAGS=""
-#NOTMUCH_FLAGS="--quiet"
+GMI_FLAGS="--quiet"
+NOTMUCH_FLAGS="--quiet"
 
 # sync mail
 cd $MAIL_ROOT
+echo "Sending emails ('gmi manage_queue')..."
 $GMI manage_queue --quiet -r -C ~/.mail/account.gmail
+echo "Fetching emails ('gmi sync')..."
 $GMI sync $GMI_FLAGS
+echo "Filtering emails ('notmuch new')..."
 notmuch new $NOTMUCH_FLAGS
 
 echo $(date "+%Y-%m-%d %H:%M") "*** DONE *** "
