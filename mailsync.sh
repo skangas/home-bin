8c406a683a731d917ce9a95f558456bd3c64c0ed
Stefan Kangas <stefankangas@gmail.com>
2022-12-01 20:39:08 +0100

    Fix mailsync.sh

--- mailsync.sh
+++ mailsync.sh
@@ -15,24 +15,60 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+# configuration
+MAIL_ROOT="$HOME/.mail/account.gmail/"
+GMI="$HOME/src/lieer/gmi"
+
 # lock -- see flock(1) for more details
 [ "${FLOCKER}" != "$0" ] && exec env FLOCKER="$0" flock -en "$0" "$0" "$@" || :
 
-# configuration
-MAIL_ROOT=~/.mail/account.gmail/
-GMI=$HOME/src/lieer/gmi
-GMI_FLAGS="--quiet"
-NOTMUCH_FLAGS="--quiet"
+# Treat unset variables as an error.
+set -o nounset
 
-echo $(date "+%Y-%m-%d %H:%M:%S") "*** START *** "
+# Exit immediately on error.
+set -o errexit
 
-# sync mail
+GREEN='\033[0;32m'
+NC='\033[0m' # No color
+
+die ()                 # write error to stderr and exit
+{
+    [ $# -gt 0 ] && echo "$PN: $@" >&2
+    exit 1
+}
+
+print_log ()
+{
+    echo -e "$GREEN$(date "+%Y-%m-%d %H:%M:%S") $@$NC"
+}
+
+gmi_flags=""
+notmuch_flags=""
+
+while getopts "q" option ; do
+    case $option in
+        (q) gmi_flags="--quiet"
+            notmuch_flags="--quiet"
+            ;;
+
+        (*) die "getopts error" ;;
+    esac
+done
+
+print_log '*** START ***'
+
 cd $MAIL_ROOT
-echo "Sending emails ('gmi manage_queue')..."
-$GMI manage_queue --quiet -r -C ~/.mail/account.gmail
-echo "Fetching emails ('gmi sync')..."
-$GMI sync $GMI_FLAGS
-echo "Filtering emails ('notmuch new')..."
-notmuch new $NOTMUCH_FLAGS
 
-echo $(date "+%Y-%m-%d %H:%M:%S") "*** DONE *** "
+# send emails
+print_log "Sending emails ('gmi manage_queue')..."
+torsocks $GMI manage_queue -r -C ~/.mail/account.gmail
+
+# fetch emails
+print_log "Fetching emails ('gmi sync')..."
+torsocks $GMI sync $gmi_flags
+
+# filter emails with notmuch
+print_log "Filtering emails ('notmuch new')..."
+notmuch new $notmuch_flags
+
+print_log '*** DONE ***'
